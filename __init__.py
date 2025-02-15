83d6e98599f73d44537b5db07ad345e7d6964edf
kanta <101532773+kantas-spike@users.noreply.github.com>
2025-02-15 13:24:25 +0900

    オペレーターの雛形を追加

--- __init__.py
+++ __init__.py
@@ -11,6 +11,28 @@ bl_info = {
 }
 
 
+class CropmanAddPlaceholder(bpy.types.Operator):
+    bl_idname = "cropman.add_placeholder"
+    bl_label = "placeholderを追加"
+    bl_description = "Crop範囲を表すplaceholderを追加します。"
+    bl_options = {"REGISTER", "UNDO"}
+
+    def execute(self, context):
+        print(f"{self.bl_idname}を実行しました。")
+        return {"FINISHED"}
+
+
+class CropmanCropAllPlaceholders(bpy.types.Operator):
+    bl_idname = "cropman.crop_all_placeholder"
+    bl_label = "全てのplaceholderを切り抜く"
+    bl_description = "全てのplaceholderをCropしたstripに置き換えます。"
+    bl_options = {"REGISTER", "UNDO"}
+
+    def execute(self, context):
+        print(f"{self.bl_idname}を実行しました。")
+        return {"FINISHED"}
+
+
 class CropmanMainPanel(bpy.types.Panel):
     bl_space_type = "SEQUENCE_EDITOR"
     bl_region_type = "UI"
@@ -26,10 +48,12 @@ class CropmanMainPanel(bpy.types.Panel):
         layout = self.layout
         layout.label(text="Crop対象ストリップの選択")
         layout.label(text="Placeholder操作")
+        layout.operator(CropmanAddPlaceholder.bl_idname)
         layout.label(text="Crop操作")
+        layout.operator(CropmanCropAllPlaceholders.bl_idname)
 
 
-classes = [CropmanMainPanel]
+classes = [CropmanAddPlaceholder, CropmanCropAllPlaceholders, CropmanMainPanel]
 
 
 def register():
