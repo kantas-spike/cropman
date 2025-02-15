b73f8c7edd67a39e1287958fa56ce2619363721b
kanta <101532773+kantas-spike@users.noreply.github.com>
2025-02-15 13:08:07 +0900

    UIの雛形を追加

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
+import bpy
+
 bl_info = {
     "name": "Cropman",
     "description": "VSEで指定サイズにクロップしたtransformストリップを追加する。",
@@ -9,9 +11,36 @@ bl_info = {
 }
 
 
+class CropmanMainPanel(bpy.types.Panel):
+    bl_space_type = "SEQUENCE_EDITOR"
+    bl_region_type = "UI"
+    bl_category = "Cropman"
+    bl_label = "Cropman"
+    bl_idname = "CROPMAN_PT_MainPanel"
+
+    @classmethod
+    def poll(cls, context):
+        return context.space_data.view_type == "SEQUENCER"
+
+    def draw(self, context):
+        layout = self.layout
+        layout.label(text="Crop対象ストリップの選択")
+        layout.label(text="Placeholder操作")
+        layout.label(text="Crop操作")
+
+
+classes = [CropmanMainPanel]
+
+
 def register():
+    for c in classes:
+        bpy.utils.register_class(c)
+
     print(f"{bl_info['name']}が有効化されました")
 
 
 def unregister():
+    for c in reversed(classes):
+        bpy.utils.unregister_class(c)
+
     print(f"{bl_info['name']}が無効化されました")
